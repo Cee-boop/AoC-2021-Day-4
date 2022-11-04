06b57f6b9b242dfec72f7f0eb8540d51b48bf169
Chantelle <79821347+Cee-boop@users.noreply.github.com>
2022-11-04 18:49:57 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 with open(file='data.txt') as file:
     numbers, board_breakdown = [int(num) for num in file.readline().strip().split(",")], [row.rstrip().lstrip().split(" ") for row in file.read().strip().split("\n") if len(row) > 1]
-    boards = []
+    data = []
     for index, row in enumerate(board_breakdown):
-        boards.append([])
+        data.append([])
         for number in row:
             if number:
-                boards[index].append(int(number))
+                data[index].append(int(number))
 
 #####################################################################
 BINGO_NUMBERS = numbers
@@ -15,34 +15,22 @@ COL_LENGTH = 5
 
 class BingoBoard:
 
-    def __init__(self, board_data, board_index_pos):
-        self.board = [board_data[row] for row in range(len(board_data))]
-        self.board_index_pos = board_index_pos
-        self.winning_board = False
+    def __init__(self, board_data, board_index):
+        self.template = [board_data[row] for row in range(len(board_data))]
+        self.board_index = board_index
+        self.is_winning_board = False
         self.is_faster = False
         self.is_slower = False
-        self.call_numbers()
 
-    def call_numbers(self):
-        global fastest_game, slowest_game
-        for i, call_out in enumerate(BINGO_NUMBERS):
-            for r, row in enumerate(self.board):
-                for c, n in enumerate(row):
-
-                    if self.board[r][c] == call_out:
-                        self.board[r][c] = "X"
-                        self.check_row(i)
-                        self.check_column(i)
-                        if self.winning_board:
-                            self.winning_board_sum()
-                            print(f"board: {self.board}, "
-                                  f"board count: {self.board_index_pos}, "
-                                  f"fastest game: {fastest_game}, "
-                                  f"slowest game: {slowest_game}")
-                            return
+    def search(self, n):
+        for r, row in enumerate(self.template):
+            for c, col in enumerate(row):
+                if self.template[r][c] == n:
+                    # if number present, mark board
+                    self.template[r][c] = "X"
 
     def check_row(self, number_index):
-        for row in self.board:
+        for row in self.template:
             if row.count("X") == 5:
                 self.log_results(number_index)
                 return
@@ -51,7 +39,7 @@ class BingoBoard:
         for c in range(ROW_LENGTH):
             column = []
             for r in range(COL_LENGTH):
-                column.append(self.board[r][c])
+                column.append(self.template[r][c])
                 if column.count("X") == 5:
                     self.log_results(number_index)
                     return
@@ -62,21 +50,21 @@ class BingoBoard:
             self.is_faster = True
             fastest_game['call_index'] = number_index
             fastest_game['winning_number'] = BINGO_NUMBERS[number_index]
-            fastest_game['board_number'] = self.board_index_pos
+            fastest_game['board_number'] = self.board_index
         if number_index > slowest_game['call_index']:
             self.is_slower = True
             slowest_game['call_index'] = number_index
             slowest_game['winning_number'] = BINGO_NUMBERS[number_index]
-            slowest_game['board_number'] = self.board_index_pos
-        self.winning_board = True
+            slowest_game['board_number'] = self.board_index
+        self.is_winning_board = True
 
-    def winning_board_sum(self):
+    def winning_sum(self):
         global fastest_game, slowest_game
         board_sum = 0
-        for r, row in enumerate(self.board):
+        for r, row in enumerate(self.template):
             for c, col in enumerate(row):
-                if self.board[r][c] != "X":
-                    board_sum += self.board[r][c]
+                if self.template[r][c] != "X":
+                    board_sum += self.template[r][c]
 
         if self.is_faster:
             fastest_game['board_sum'] = board_sum * fastest_game['winning_number']
@@ -89,12 +77,28 @@ slowest_game = {'board_number': float('-inf'), 'call_index': float('-inf'), 'win
 
 i, j = 0, 5  # board parsing window
 curr_board_index = 0
-while boards:
-    BingoBoard(boards[i:j], curr_board_index)
-    boards = boards[j:]
+bingo_boards = []
+while data:
+    new_board = BingoBoard(data[i:j], curr_board_index)
+    bingo_boards.append(new_board)
+    data = data[j:]
     curr_board_index += 1
+
+for curr_board in bingo_boards:
+    # start calling numbers...
+    for i, number in enumerate(BINGO_NUMBERS):
+        curr_board.search(number)
+        curr_board.check_row(i)
+        curr_board.check_column(i)
+        if curr_board.is_winning_board:
+            curr_board.winning_sum()
+            print(f"board: {curr_board.template} board number: {curr_board.board_index}, "
+                  f"fastest game: {fastest_game}, slowest game: {slowest_game}")
+            break
+
 
 # part one:
 print(fastest_game['board_sum'])
 # part two:
 print(slowest_game['board_sum'])
+
